e1c5d747f6453e5d363d0f8c09b4b10e1ed25c4e
nitnub <45341924+nitnub@users.noreply.github.com>
2024-06-04 21:58:12 -0700

    general cleanup and add time and related sorting to calendar entries

--- FoodTracker/obj/Debug/net8.0/FoodTrackerWeb.AssemblyInfo.cs
+++ FoodTracker/obj/Debug/net8.0/FoodTrackerWeb.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodTrackerWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0d026809d147a64018d4ee53bebb90b16f084a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddabe50b9194837e860124140f7142d46b1293db")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodTrackerWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodTrackerWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
